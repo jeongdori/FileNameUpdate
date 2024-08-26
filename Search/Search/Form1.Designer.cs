0e441aafd4a68d542f73c46bcf8978c9fd6d31b6
jeongdori <goody12290@gmail.com>
2024-08-26 13:39:33 +0900

    init

---
+++ Search/Search/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            groupBox2 = new GroupBox();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            fileSystemWatcher1 = new FileSystemWatcher();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button2 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(107, 120);
+            button1.Name = "button1";
+            button1.Size = new Size(208, 32);
+            button1.TabIndex = 0;
+            button1.Text = "파일명 추가";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(379, 22);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(409, 409);
+            listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(73, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(300, 23);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 15);
+            label1.TabIndex = 3;
+            label1.Text = "루트경로";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(12, 73);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(89, 79);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "앞뒤";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(6, 47);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(77, 19);
+            radioButton2.TabIndex = 6;
+            radioButton2.Text = "뒤에 추가";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Checked = true;
+            radioButton1.Location = new Point(6, 22);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(77, 19);
+            radioButton1.TabIndex = 5;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "앞에 추가";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(107, 82);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "파일명에 추가할 텍스트";
+            textBox2.Size = new Size(208, 23);
+            textBox2.TabIndex = 5;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(107, 204);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "제거할 텍스트 길이";
+            textBox3.Size = new Size(208, 23);
+            textBox3.TabIndex = 7;
+            textBox3.TextChanged += textBox3_TextChanged;
+            textBox3.KeyPress += textBox3_KeyPress;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(radioButton3);
+            groupBox2.Controls.Add(radioButton4);
+            groupBox2.Location = new Point(12, 195);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(89, 79);
+            groupBox2.TabIndex = 6;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "앞뒤";
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(6, 47);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(77, 19);
+            radioButton3.TabIndex = 6;
+            radioButton3.Text = "뒤에 제거";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Checked = true;
+            radioButton4.Location = new Point(6, 22);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(77, 19);
+            radioButton4.TabIndex = 5;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "앞에 제거";
+            radioButton4.UseVisualStyleBackColor = true;
+            radioButton4.CheckedChanged += radioButton4_CheckedChanged;
+            // 
+            // fileSystemWatcher1
+            // 
+            fileSystemWatcher1.EnableRaisingEvents = true;
+            fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(379, 4);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 8;
+            label2.Text = "결과창";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 55);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 15);
+            label3.TabIndex = 9;
+            label3.Text = "파일명 추가";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(17, 177);
+            label4.Name = "label4";
+            label4.Size = new Size(71, 15);
+            label4.TabIndex = 10;
+            label4.Text = "파일명 제거";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(107, 241);
+            button2.Name = "button2";
+            button2.Size = new Size(208, 33);
+            button2.TabIndex = 11;
+            button2.Text = "파일명 제거";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(textBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Label label1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private GroupBox groupBox2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private FileSystemWatcher fileSystemWatcher1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Button button2;
+    }
+}
---
+++ Search/Search/Form1.cs
@@ -0,0 +1,258 @@
+using OfficeOpenXml;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Search
+{
+    public partial class Form1 : Form
+    {
+        // 원본 파일 루트
+        string rootPath = null;
+        Boolean isAddFront = true;
+        Boolean isRemoveFront = true;
+        string addText = String.Empty;
+        int removeTextLength = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        }
+
+        #region 파일명 추가
+
+        // 루트경로
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (sender is TextBox textbox)
+            {
+                rootPath = textbox.Text;
+            }
+        }
+
+        // 추가 텍스트
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (sender is TextBox textbox)
+            {
+                string inputText = textbox.Text;
+
+                if (ContainsInvalidFileNameChars(inputText))
+                {
+                    MessageBox.Show("' \\ / : * ? \" < > | ' \r 이 문자들은 파일명에 사용할 수 없단다^^  ", "경고", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    textbox.Text = String.Empty;
+                }
+                else
+                {
+                    addText = inputText;
+                }
+            }
+        }
+
+        // 추가 라디오
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                isAddFront = radioButton.Checked;
+            }
+
+        }
+
+        // 파일명 추가
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessFiles(GetNewFileName);
+        }
+
+        private string GetNewFileName(string fileName)
+        {
+            // 파일명 수정 로직
+            var match = Regex.Match(fileName, @"^(.*)(\.tif(?:\.LDH)?)$");
+            if (match.Success)
+            {
+                if (isAddFront)
+                    return $"{addText}{match.Groups[1].Value}{match.Groups[2].Value}";
+                else
+                    return $"{match.Groups[1].Value}{addText}{match.Groups[2].Value}";
+            }
+            return fileName;
+        }
+
+        #endregion
+
+
+        #region 파일명 제거
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // 숫자키와 백스페이스 키 허용
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+            // 첫 번째 문자가 '0'인지 확인
+            if (sender is TextBox textbox && textbox.Text.Length == 0 && e.KeyChar == '0')
+            {
+                e.Handled = true;
+            }
+        }
+        // 제거 텍스트 길이
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (sender is TextBox textbox)
+            {
+                int length;
+                if (int.TryParse(textbox.Text, out length))
+                    removeTextLength = length;
+                else if (textbox.Text == String.Empty)
+                    removeTextLength = 0;
+                else
+                {
+                    MessageBox.Show("정수타입의 숫자만 입력하거라");
+                    return;
+                }
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                isRemoveFront = radioButton.Checked;
+            }
+        }
+
+        // 파일명 제거
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProcessFiles(GetRemovedFileName);
+        }
+
+        private string GetRemovedFileName(string fileName)
+        {
+            // 파일명에서 확장자를 분리
+            var match = Regex.Match(fileName, @"^(.*)(\.tif(?:\.LDH)?)$");
+            if (!match.Success)
+                return fileName;  // 형식이 맞지 않으면 원래 파일명을 반환
+
+            string nameWithoutExtension = match.Groups[1].Value;
+            string extension = match.Groups[2].Value;
+
+            if (nameWithoutExtension.Length <= removeTextLength)
+            {
+                MessageBox.Show("제거하려는 길이가 파일명보다 깁니다.");
+                return fileName;
+            }
+
+            if (isRemoveFront)
+            {
+                return nameWithoutExtension.Substring(removeTextLength) + extension;
+            }
+            else
+            {
+                return nameWithoutExtension.Substring(0, nameWithoutExtension.Length - removeTextLength) + extension;
+            }
+        }
+
+
+
+        #endregion
+
+
+        #region 함수
+
+        private void ProcessFiles(Func<string, string> fileNameModificationFunc)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(rootPath))
+                {
+                    MessageBox.Show("루트경로를 입력하거라");
+                    return;
+                }
+
+                if (!Directory.Exists(rootPath))
+                {
+                    MessageBox.Show("지정된 경로가 존재하지 않구나");
+                    return;
+                }
+
+                var result = new ConcurrentBag<(string FolderName, string OldFileName, string NewFileName)>();
+
+                var directories = Directory.GetDirectories(rootPath);
+
+                if (directories.Length == 0)
+                {
+                    MessageBox.Show("하위 디렉토리가 존재하지 않구나");
+                    return;
+                }
+
+                Parallel.ForEach(directories, dir =>
+                {
+                    var files = Directory.GetFiles(dir, "*.*");
+                    if (files.Length == 0)
+                    {
+                        result.Add((Path.GetFileName(dir), "No Files", "No Files"));
+                    }
+                    else
+                    {
+                        foreach (var file in files)
+                        {
+                            string fileName = Path.GetFileName(file);
+                            string newFileName = fileNameModificationFunc(fileName);
+                            if (newFileName != fileName)
+                            {
+                                string newPath = Path.Combine(dir, newFileName);
+                                File.Move(file, newPath);
+                                result.Add((Path.GetFileName(dir), fileName, newFileName));
+                            }
+                        }
+                    }
+                });
+
+                var sortedResult = result.OrderBy(r => r.FolderName).ThenBy(r => r.OldFileName).ToList();
+
+                // ListBox에 결과 표시
+                listBox1.Invoke(new Action(() =>
+                {
+                    listBox1.Items.Clear();
+                    foreach (var item in sortedResult)
+                    {
+                        listBox1.Items.Add($"폴더명: {item.FolderName},  원본 파일명: {item.OldFileName},  수정된 파일명: {item.NewFileName}");
+                    }
+                }));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"오류: {ex.Message}");
+            }
+        }
+
+
+        // 윈도우 파일명에 금지된 특수문자 확인
+        // \ / : * ? " < > |
+        private bool ContainsInvalidFileNameChars(string text)
+        {
+            char[] invalidChars = Path.GetInvalidFileNameChars();
+            foreach (char c in text)
+            {
+                if (invalidChars.Contains(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        #endregion
+
+
+
+
+
+
+
+    }
+}
